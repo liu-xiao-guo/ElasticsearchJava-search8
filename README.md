ecae9aaae7f76215cfb99f8a2de38900201456f2
Xiaoguo Liu <liu_xiao_guo@yahoo.com>
2022-03-30 10:19:56 +0800

    Initial release

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a simple demo showing how to use the last Elasticsearch Java client 8.0 to connect to Elasticsearch and do indexing/searching. 
---
+++ src/main/java/ElasticsearchJava.java
@@ -0,0 +1,149 @@
+import co.elastic.clients.elasticsearch.ElasticsearchAsyncClient;
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.elasticsearch._types.query_dsl.QueryBuilders;
+import co.elastic.clients.elasticsearch._types.query_dsl.TermQuery;
+import co.elastic.clients.elasticsearch.core.*;
+import co.elastic.clients.elasticsearch.core.search.Hit;
+import co.elastic.clients.json.jackson.JacksonJsonpMapper;
+import co.elastic.clients.transport.ElasticsearchTransport;
+import co.elastic.clients.transport.rest_client.RestClientTransport;
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
+
+import java.io.IOException;
+
+public class ElasticsearchJava {
+
+    private static ElasticsearchClient client = null;
+    private static ElasticsearchAsyncClient asyncClient = null;
+
+    private static synchronized void makeConnection() {
+        // Create the low-level client
+        final CredentialsProvider credentialsProvider =
+                new BasicCredentialsProvider();
+        credentialsProvider.setCredentials(AuthScope.ANY,
+                new UsernamePasswordCredentials("elastic", "password"));
+
+        RestClientBuilder builder = RestClient.builder(
+                        new HttpHost("localhost", 9200))
+                .setHttpClientConfigCallback(new RestClientBuilder.HttpClientConfigCallback() {
+                    @Override
+                    public HttpAsyncClientBuilder customizeHttpClient(
+                            HttpAsyncClientBuilder httpClientBuilder) {
+                        return httpClientBuilder
+                                .setDefaultCredentialsProvider(credentialsProvider);
+                    }
+                });
+
+        RestClient restClient = builder.build();
+
+        // Create the transport with a Jackson mapper
+        ElasticsearchTransport transport = new RestClientTransport(
+                restClient, new JacksonJsonpMapper());
+
+        // And create the API client
+        client = new ElasticsearchClient(transport);
+        asyncClient = new ElasticsearchAsyncClient(transport);
+    }
+
+    public static void main(String[] args) throws IOException {
+        makeConnection();
+
+        // Index data to an index products
+        Product product = new Product("abc", "Bag", 42);
+
+        IndexRequest<Object> indexRequest = new IndexRequest.Builder<>()
+                .index("products")
+                .id("abc")
+                .document(product)
+                .build();
+
+        client.index(indexRequest);
+
+        Product product1 = new Product("efg", "Bag", 42);
+
+        client.index(builder -> builder
+                .index("products")
+                .id(product1.getId())
+                .document(product1)
+        );
+
+        // Search for a data
+        TermQuery query = QueryBuilders.term()
+                .field("name")
+                .value("bag")
+                .build();
+
+        SearchRequest request = new SearchRequest.Builder()
+                .index("products")
+                .query(query._toQuery())
+                .build();
+
+        SearchResponse<Product> search =
+                client.search(
+                        request,
+                        Product.class
+                );
+
+        for (Hit<Product> hit: search.hits().hits()) {
+            Product pd = hit.source();
+            System.out.println(pd);
+        }
+
+        SearchResponse<Product> search1 = client.search(s -> s
+                        .index("products")
+                        .query(q -> q
+                                .term(t -> t
+                                        .field("name")
+                                        .value(v -> v.stringValue("bag"))
+                                )),
+                Product.class);
+
+        for (Hit<Product> hit: search1.hits().hits()) {
+            Product pd = hit.source();
+            System.out.println(pd);
+        }
+
+        // Splitting complex DSL
+        TermQuery termQuery = TermQuery.of(t ->t.field("name").value("bag"));
+
+        SearchResponse<Product> search2 = client.search(s -> s
+                .index("products")
+                .query(termQuery._toQuery()),
+                Product.class
+        );
+
+        for (Hit<Product> hit: search2.hits().hits()) {
+            Product pd = hit.source();
+            System.out.println(pd);
+        }
+
+        // Creating aggregations
+        SearchResponse<Void> search3 = client.search( b-> b
+                .index("products")
+                .size(0)
+                .aggregations("price-histo", a -> a
+                        .histogram(h -> h
+                                .field("price")
+                                .interval(20.0)
+                        )
+                ),
+                Void.class
+        );
+
+        long firstBucketCount = search3.aggregations()
+                .get("price-histo")
+                .histogram()
+                .buckets().array()
+                .get(0)
+                .docCount();
+
+        System.out.println("doc count: " + firstBucketCount);
+    }
+}
